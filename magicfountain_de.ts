5e7a0687a98c30c3366741d31f9e2a0569a77565
ZeroDot1 <blindgrafix@googlemail.com>
2017-08-23 22:23:38 +0200

    Update magicfountain_de.ts
    German translation added.

--- magicfountain_de.ts
+++ magicfountain_de.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_DE">
+<TS version="2.0" language="de_DE">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
         <source>MainWindow</source>
-        <translation type="unfinished"></translation>
+        <translation>Hauptfenster</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="32"/>
         <source>Fountain Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>Fountain Editor</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="49"/>
         <source>Preview</source>
-        <translation type="unfinished"></translation>
+        <translation>Vorschau</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="77"/>
@@ -25,147 +25,151 @@
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Courier&apos;; font-size:12pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Courier New&apos;;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Courier&apos;; font-size:12pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Courier New&apos;;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="121"/>
         <source>Fi&amp;le</source>
-        <translation type="unfinished"></translation>
+        <translation>Datei</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="135"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Bearbeiten</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="143"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Hilfe</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="156"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation>toolBar</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="196"/>
         <source>&amp;Open</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Öffnen...</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="205"/>
         <source>&amp;Save</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Speichern</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="214"/>
         <source>Sa&amp;ve as ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Speichern als...</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="223"/>
         <source>&amp;Export as PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Als PDF exportieren...</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="232"/>
         <source>&amp;Print</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Drucken</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="241"/>
         <source>&amp;Bold</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Fett</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="244"/>
         <source>Bold</source>
-        <translation type="unfinished"></translation>
+        <translation>Fett</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="253"/>
         <source>&amp;Italic</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Kursiv</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="256"/>
         <source>Italic</source>
-        <translation type="unfinished"></translation>
+        <translation>Kursiv</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="265"/>
         <source>&amp;Underline</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Unterstrichen</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="268"/>
         <source>Underline</source>
-        <translation type="unfinished"></translation>
+        <translation>Unterstrichen</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="277"/>
         <source>Export as &amp;HTML</source>
-        <translation type="unfinished"></translation>
+        <translation>Als HTML exportieren</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="280"/>
         <source>Export as HTML</source>
-        <translation type="unfinished"></translation>
+        <translation>Als HTML exportieren</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="289"/>
         <source>&amp;New</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Neu</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="292"/>
         <source>New file</source>
-        <translation type="unfinished"></translation>
+        <translation>Neue Datei</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="297"/>
         <source>&amp;New scene</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Neue Szene</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="302"/>
         <source>Fountain Syntax</source>
-        <translation type="unfinished"></translation>
+        <translation>Fountain Syntax</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="307"/>
         <source>About Magic Fountain</source>
-        <translation type="unfinished"></translation>
+        <translation>Über Magic Fountain</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="312"/>
         <source>About Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>Über Qt</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="91"/>
         <source>Export PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>Als PDF exportieren</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="114"/>
         <source>Export HTML</source>
-        <translation type="unfinished"></translation>
+        <translation>Als HTML Exportieren</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="140"/>
         <source>Print Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Dokument drucken</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="170"/>
         <source>Open Fountain file</source>
-        <translation type="unfinished"></translation>
+        <translation>Fountain Datei öffenen</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="192"/>
         <source>Save Fountain file</source>
-        <translation type="unfinished"></translation>
+        <translation>Fountain Datei ppeichern</translation>
     </message>
 </context>
 </TS>
